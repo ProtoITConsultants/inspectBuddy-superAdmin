40f394fd4650c711bac4589515556d44b02d6a50
Babar78 <muhammadbabarashraf8@gmail.com>
2025-03-28 23:20:00 +0500

    feat: add state and setter for selected inspection room elements in inspection store

--- src/store/inspectionStore.js
+++ src/store/inspectionStore.js
@@ -4,4 +4,9 @@ export const useInspectionStore = create((set) => ({
   // State to store the Inspection Rooms
   inspectionRooms: [],
   setInspectionRooms: (inspectionRooms) => set({ inspectionRooms }),
+
+  // Selected Template Room Elements
+  selectedInspectionRoomElements: [],
+  setSelectedInspectionRoomElements: (selectedInspectionRoomElements) =>
+    set({ selectedInspectionRoomElements }),
 }));
