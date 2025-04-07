86a286fbdbde849ab3057fcabc92db712f666c7e
Babar78 <muhammadbabarashraf8@gmail.com>
2025-04-08 00:23:14 +0500

    feat: update DashboardSkeleton layout for improved responsiveness and structure

--- src/features/dashboard/components/DashboardSkeleton.jsx
+++ src/features/dashboard/components/DashboardSkeleton.jsx
@@ -3,7 +3,7 @@ import { Skeleton } from "@mantine/core";
 const DashboardSkeleton = () => {
   return (
     <div className="flex flex-col gap-[20px]">
-      <div className="flex items-center gap-[25px]">
+      <div className="flex flex-wrap items-center gap-[25px] sm:justify-start justify-center">
         {Array(4)
           .fill("")
           .map((_, index) => (
@@ -15,7 +15,7 @@ const DashboardSkeleton = () => {
             />
           ))}
       </div>
-      <div className="grid grid-cols-2 gap-[20px] mt-[20px] max-w-[1395px] w-full">
+      <div className="grid md:grid-cols-2 grid-cols-1 gap-[20px] mt-[20px] max-w-[1395px] w-full">
         <Skeleton height={345} radius={8} className="w-full" />
         <Skeleton height={246} radius={8} className="w-full" />
       </div>
