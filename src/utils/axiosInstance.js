604cf6199ba5540fdad460e051c62406d74d6e97
Babar78 <muhammadbabarashraf8@gmail.com>
2025-04-10 01:43:07 +0500

    feat: refactor axios instance to use dynamic base URL based on environment

--- src/utils/axiosInstance.js
+++ src/utils/axiosInstance.js
@@ -1,7 +1,13 @@
 import axios from "axios";
 
+// If node env is production, use the production URL, otherwise use the development URL
+const BASE_URL =
+  import.meta.env.NODE_ENV === "production"
+    ? import.meta.env.VITE_DEPLOYED_URL
+    : import.meta.env.VITE_BASE_URL;
+
 const axiosInstance = axios.create({
-  baseURL: import.meta.env.VITE_BASE_URL,
+  baseURL: BASE_URL,
   withCredentials: true, // Enable cookies for all requests
 });
 
