8ab9c6a59b091012aa1657f6c623c9d725d09f07
Babar78 <muhammadbabarashraf8@gmail.com>
2025-04-19 02:43:19 +0500

    refactor: remove unused axios request module instance

--- src/utils/axiosRequestModuleInstance.js
+++
@@ -1,8 +0,0 @@
-import axios from "axios";
-
-const axiosInstanceForRequests = axios.create({
-  baseURL: import.meta.env.VITE_DEPLOYED_URL_FOR_USER_REQUESTS,
-  withCredentials: true, // Enable cookies for all requests
-});
-
-export default axiosInstanceForRequests;
