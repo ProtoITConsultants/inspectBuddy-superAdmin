d72cf8ad9b7e489330af0c23866e7ca26ed1c959
Babar78 <muhammadbabarashraf8@gmail.com>
2025-04-21 20:47:51 +0500

    refactor: simplify RestoreRequests component by removing tabs and streamlining table rendering

--- src/pages/restore-requests/RestoreRequests.jsx
+++ src/pages/restore-requests/RestoreRequests.jsx
@@ -1,4 +1,3 @@
-import { Tabs } from "@mantine/core";
 import Table from "../../components/ui/Table";
 import { USER_REQUESTS_TABLE_HEADINGS } from "../../constants/tables/headings";
 import { useQuery } from "@tanstack/react-query";
@@ -10,9 +9,6 @@ import { userRestoreRequestsAPIs } from "../../features/user-requests/api";
 import { toast } from "sonner";
 
 const RestoreRequests = () => {
-  // Local States
-  const [activeTab, setActiveTab] = useState("TEMPLATE");
-
   // Filters Data
   const [filtersData, setFiltersData] = useState({
     search: "",
@@ -21,11 +17,10 @@ const RestoreRequests = () => {
 
   // Fetch User Requests Query
   const { data, isError, error, isPending } = useQuery({
-    queryKey: ["userRequestsQuery", filtersData, activeTab],
+    queryKey: ["userRequestsQuery", filtersData],
     queryFn: () =>
       userRestoreRequestsAPIs.fetchUsersWithRequests({
         filtersData,
-        type: activeTab,
       }),
   });
 
@@ -56,7 +51,7 @@ const RestoreRequests = () => {
         <Table.DoubleColumn className="justify-end">
           <IconLink
             id="view-restore-request-details"
-            href={`details/${user._id}?type=${activeTab}`}
+            href={`details/${user._id}?type=templates`}
             icon={<VIEW_DETAIL_ICON className="h-[16px]" />}
             label="View Details"
           />
@@ -66,179 +61,54 @@ const RestoreRequests = () => {
   ));
 
   return (
-    <Tabs
-      value={activeTab}
-      onChange={(value) => setActiveTab(value)}
-      keepMounted={false}
-      color="#2A85FF"
-      classNames={{
-        root: "h-full",
-        list: "w-fit",
-        panel: "h-[calc(100%-60px)]",
-      }}
-    >
-      <Tabs.List className="mb-[24px]">
-        <Tabs.Tab value="TEMPLATE">For Templates</Tabs.Tab>
-        <Tabs.Tab value="INSPECTION">For Inspections</Tabs.Tab>
-        <Tabs.Tab value="PROPERTY">For Properties</Tabs.Tab>
-      </Tabs.List>
+    <Table.Root className="lg:p-[16px] h-full user-requests-table-root">
+      {/* Table Header */}
+      <Table.Header>
+        {USER_REQUESTS_TABLE_HEADINGS.map((heading) =>
+          heading.key !== "serialNo" ? (
+            <Table.DoubleColumn key={heading.key}>
+              <Table.HeaderItem heading={heading.value} />
+            </Table.DoubleColumn>
+          ) : (
+            <Table.SingleColumn key={heading.key}>
+              <Table.HeaderItem heading={heading.value} />
+            </Table.SingleColumn>
+          )
+        )}
+      </Table.Header>
+      {/* Table Body */}
+      <Table.Body
+        className={`${
+          data?.totalPages < 2
+            ? "h-[calc(100%-96.8px)]"
+            : "h-[calc(100%-106.8px)]"
+        }`}
+      >
+        {isPending ? (
+          <TableSkeleton />
+        ) : data?.users?.length < 1 ? (
+          <div className="flex justify-center items-center h-full">
+            <p className="text-[14px] font-medium text-[#6C727F] text-center">
+              No User Request Found for restoring templates.
+            </p>
+          </div>
+        ) : (
+          rows
+        )}
+      </Table.Body>
 
-      <Tabs.Panel value="TEMPLATE">
-        {/* User Requests Table - Templates*/}
-        <Table.Root className="lg:p-[16px] h-full user-requests-table-root">
-          {/* Table Header */}
-          <Table.Header>
-            {USER_REQUESTS_TABLE_HEADINGS.map((heading) =>
-              heading.key !== "serialNo" ? (
-                <Table.DoubleColumn key={heading.key}>
-                  <Table.HeaderItem heading={heading.value} />
-                </Table.DoubleColumn>
-              ) : (
-                <Table.SingleColumn key={heading.key}>
-                  <Table.HeaderItem heading={heading.value} />
-                </Table.SingleColumn>
-              )
-            )}
-          </Table.Header>
-          {/* Table Body */}
-          <Table.Body
-            className={`${
-              data?.totalPages < 2
-                ? "h-[calc(100%-96.8px)]"
-                : "h-[calc(100%-106.8px)]"
-            }`}
-          >
-            {isPending ? (
-              <TableSkeleton />
-            ) : data?.users?.length < 1 ? (
-              <div className="flex justify-center items-center h-full">
-                <p className="text-[14px] font-medium text-[#6C727F] text-center">
-                  No User Request Found for restoring templates.
-                </p>
-              </div>
-            ) : (
-              rows
-            )}
-          </Table.Body>
-
-          {data && data?.totalPages > 0 && (
-            <Table.Pagination
-              filtersData={filtersData}
-              setFiltersData={(value) => setFiltersData({ page: value.page })}
-              paginationData={{
-                totalPages: data?.totalPages,
-                currentPage: data?.currentPage,
-                totalItems: data?.totalUsers,
-              }}
-            />
-          )}
-        </Table.Root>
-      </Tabs.Panel>
-
-      <Tabs.Panel value="INSPECTION">
-        {/* User Requests Table - Inspections*/}
-        <Table.Root className="lg:p-[16px] h-full user-requests-table-root">
-          {/* Table Header */}
-          <Table.Header>
-            {USER_REQUESTS_TABLE_HEADINGS.map((heading) =>
-              heading.key !== "serialNo" ? (
-                <Table.DoubleColumn key={heading.key}>
-                  <Table.HeaderItem heading={heading.value} />
-                </Table.DoubleColumn>
-              ) : (
-                <Table.SingleColumn key={heading.key}>
-                  <Table.HeaderItem heading={heading.value} />
-                </Table.SingleColumn>
-              )
-            )}
-          </Table.Header>
-          {/* Table Body */}
-          <Table.Body
-            className={`${
-              data?.totalPages < 2
-                ? "h-[calc(100%-96.8px)]"
-                : "h-[calc(100%-106.8px)]"
-            }`}
-          >
-            {isPending ? (
-              <TableSkeleton />
-            ) : data?.users?.length < 1 ? (
-              <div className="flex justify-center items-center h-full">
-                <p className="text-[14px] font-medium text-[#6C727F] text-center">
-                  No User Request Found for restoring Inspections.
-                </p>
-              </div>
-            ) : (
-              rows
-            )}
-          </Table.Body>
-
-          {data && data?.totalPages > 0 && (
-            <Table.Pagination
-              filtersData={filtersData}
-              setFiltersData={(value) => setFiltersData({ page: value.page })}
-              paginationData={{
-                totalPages: data?.totalPages,
-                currentPage: data?.currentPage,
-                totalItems: data?.totalUsers,
-              }}
-            />
-          )}
-        </Table.Root>
-      </Tabs.Panel>
-
-      <Tabs.Panel value="PROPERTY">
-        {/* User Requests Table - Properties*/}
-        <Table.Root className="lg:p-[16px] h-full user-requests-table-root">
-          {" "}
-          {/* Table Header */}
-          <Table.Header>
-            {USER_REQUESTS_TABLE_HEADINGS.map((heading) =>
-              heading.key !== "serialNo" ? (
-                <Table.DoubleColumn key={heading.key}>
-                  <Table.HeaderItem heading={heading.value} />
-                </Table.DoubleColumn>
-              ) : (
-                <Table.SingleColumn key={heading.key}>
-                  <Table.HeaderItem heading={heading.value} />
-                </Table.SingleColumn>
-              )
-            )}
-          </Table.Header>
-          {/* Table Body */}
-          <Table.Body
-            className={`${
-              data?.totalPages < 2
-                ? "h-[calc(100%-96.8px)]"
-                : "h-[calc(100%-106.8px)]"
-            }`}
-          >
-            {isPending ? (
-              <TableSkeleton />
-            ) : data?.users?.length < 1 ? (
-              <div className="flex justify-center items-center h-full">
-                <p className="text-[14px] font-medium text-[#6C727F] text-center">
-                  No User Request Found for restoring properties.
-                </p>
-              </div>
-            ) : (
-              rows
-            )}
-          </Table.Body>
-          {data && data?.totalPages > 0 && (
-            <Table.Pagination
-              filtersData={filtersData}
-              setFiltersData={(value) => setFiltersData({ page: value.page })}
-              paginationData={{
-                totalPages: data?.totalPages,
-                currentPage: data?.currentPage,
-                totalItems: data?.totalUsers,
-              }}
-            />
-          )}
-        </Table.Root>
-      </Tabs.Panel>
-    </Tabs>
+      {data && data?.totalPages > 0 && (
+        <Table.Pagination
+          filtersData={filtersData}
+          setFiltersData={(value) => setFiltersData({ page: value.page })}
+          paginationData={{
+            totalPages: data?.totalPages,
+            currentPage: data?.currentPage,
+            totalItems: data?.totalUsers,
+          }}
+        />
+      )}
+    </Table.Root>
   );
 };
 
