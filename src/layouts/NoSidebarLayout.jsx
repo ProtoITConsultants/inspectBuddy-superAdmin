1af24b6b182160f9f7b6a85121655c84220ac42a
Babar78 <muhammadbabarashraf8@gmail.com>
2025-05-16 00:42:57 +0500

    feat: added_inspectionLogs_and_Generate_Report_Button_on_viewInspectionDetailsScreen

--- src/layouts/NoSidebarLayout.jsx
+++ src/layouts/NoSidebarLayout.jsx
@@ -5,10 +5,43 @@ import NoSidebarBackButton from "../components/ui/NoSidebarBackButton";
 import useNavbarTitle from "../hooks/useNavbarTitle";
 import { EDIT_DETAILS_ICON } from "../assets/icons/EditIcon";
 import IconLink from "../components/ui/IconLink";
+import { GENERATE_REPORT_ICON } from "./../assets/icons/DynamicIcons";
+import Button from "../components/ui/Button";
+import { useMutation } from "@tanstack/react-query";
+import { toast } from "sonner";
+import { userInspectionsAPIs } from "../features/user-details/api/user-inspections";
 
 const NoSidebarLayout = () => {
   const { pageTitle } = useNavbarTitle();
-  const { subUserId, templateId, inspectionId } = useParams();
+  const { subUserId, templateId, inspectionId, userId } = useParams();
+
+  // Generate Report PDF - Mutation
+  const generateInspectionPDF = useMutation({
+    mutationFn: () =>
+      toast.promise(
+        userInspectionsAPIs.generateInspectionPDF({
+          inspectionId,
+          userId,
+        }),
+        {
+          loading: "Generating PDF file...",
+          success: async (data) => {
+            const { url } = data;
+            const link = document.createElement("a");
+            link.href = url;
+            link.target = "_blank"; // Open in new tab
+            document.body.appendChild(link);
+            link.click();
+            document.body.removeChild(link);
+
+            return "PDF generated successfully!";
+          },
+          error: "Error generating PDF file.",
+          duration: 3000,
+          richColors: true,
+        }
+      ),
+  });
 
   return (
     <main className="App overflow-x-hidden">
@@ -34,13 +67,31 @@ const NoSidebarLayout = () => {
               label="Edit Details"
             />
           ) : pageTitle === "Inspection Details" ? (
-            <IconLink
-              href={`${inspectionId}/edit-details`}
-              icon={
-                <EDIT_DETAILS_ICON className="h-[16px] w-[16px] text-[#9EA3AE]" />
-              }
-              label="Edit Details"
-            />
+            <div className="md:flex hidden items-center gap-2">
+              <IconLink
+                href={`${inspectionId}/inspection-logs`}
+                icon={
+                  <EDIT_DETAILS_ICON className="h-[16px] w-[16px] text-[#9EA3AE]" />
+                }
+                label="Inspection Logs"
+              />
+              <IconLink
+                href={`${inspectionId}/edit-details`}
+                icon={
+                  <EDIT_DETAILS_ICON className="h-[16px] w-[16px] text-[#9EA3AE]" />
+                }
+                label="Edit Details"
+              />
+              <Button
+                icon={
+                  <GENERATE_REPORT_ICON className="h-[16px] w-[16px] text-[#9EA3AE]" />
+                }
+                buttonType="iconButton"
+                onClick={() => generateInspectionPDF.mutate()}
+                label="Generate Report"
+                className="flex items-center !gap-[8px] !p-[8px_10px] border-[1.5px] rounded-[8px] !border-[#E5E6EB] w-fit !text-dark-blue !text-[12px] h-fit !font-medium whitespace-nowrap"
+              />
+            </div>
           ) : null}
         </NoSidebarBackButton>
         <div className="w-[calc(100vw-var(--scrollbar-width))] h-full md:p-[32px] p-[20px] md:mt-[164px] mt-[140px] md:min-h-[calc(100dvh-164px)] min-h-[calc(100dvh-140px)]">
--- src/pages/user-details/user-inspections/view-inspection-details/ViewInspectionDetails.jsx
+++ src/pages/user-details/user-inspections/view-inspection-details/ViewInspectionDetails.jsx
@@ -1,19 +1,51 @@
 import { useParams } from "react-router";
 import { userInspectionsAPIs } from "../../../../features/user-details/api/user-inspections";
-import { useQuery } from "@tanstack/react-query";
+import { useMutation, useQuery } from "@tanstack/react-query";
 import { toast } from "sonner";
 import ViewTemplateSkeleton from "../../../../features/user-details/components/templates/details/ViewTemplateSkeleton";
 import DetailPagesRoot from "../../../../features/user-details/components/DetailPagesRoot";
 import ViewInspection from "../../../../features/user-details/components/inspections/details/ViewInspectionDetails";
 import { useEffect, useState } from "react";
+import IconLink from "../../../../components/ui/IconLink";
+import { EDIT_DETAILS_ICON } from "../../../../assets/icons/EditIcon";
+import Button from "../../../../components/ui/Button";
+import { GENERATE_REPORT_ICON } from "../../../../assets/icons/DynamicIcons";
 
 const ViewInspectionDetails = () => {
   // Hooks
-  const { inspectionId } = useParams();
+  const { inspectionId, userId } = useParams();
 
   // local States
   const [completedRooms, setCompletedRooms] = useState([]);
   const [inProgressRooms, setInProgressRooms] = useState([]);
+
+  // Generate Report PDF - Mutation
+  const generateInspectionPDF = useMutation({
+    mutationFn: () =>
+      toast.promise(
+        userInspectionsAPIs.generateInspectionPDF({
+          inspectionId,
+          userId,
+        }),
+        {
+          loading: "Generating PDF file...",
+          success: async (data) => {
+            const { url } = data;
+            const link = document.createElement("a");
+            link.href = url;
+            link.target = "_blank"; // Open in new tab
+            document.body.appendChild(link);
+            link.click();
+            document.body.removeChild(link);
+
+            return "PDF generated successfully!";
+          },
+          error: "Error generating PDF file.",
+          duration: 3000,
+          richColors: true,
+        }
+      ),
+  });
 
   // Query To Fetch Inspection Details
   const {
@@ -63,6 +95,33 @@ const ViewInspectionDetails = () => {
 
   return (
     <DetailPagesRoot className="!overflow-hidden !h-full">
+      <div className="md:hidden flex items-center sm:justify-center mb-4 gap-2 overflow-scroll">
+        <IconLink
+          href={`inspection-logs`}
+          icon={
+            <EDIT_DETAILS_ICON className="h-[16px] w-[16px] text-[#9EA3AE]" />
+          }
+          label="Inspection Logs"
+          className="whitespace-nowrap"
+        />
+        <IconLink
+          href={`edit-details`}
+          icon={
+            <EDIT_DETAILS_ICON className="h-[16px] w-[16px] text-[#9EA3AE]" />
+          }
+          label="Edit Details"
+          className="whitespace-nowrap"
+        />
+        <Button
+          icon={
+            <GENERATE_REPORT_ICON className="h-[16px] w-[16px] text-[#9EA3AE]" />
+          }
+          buttonType="iconButton"
+          onClick={() => generateInspectionPDF.mutate()}
+          label="Generate Report"
+          className="flex items-center !gap-[8px] !p-[8px_10px] border-[1.5px] rounded-[8px] !border-[#E5E6EB] w-fit !text-dark-blue !text-[12px] h-fit !font-medium whitespace-nowrap"
+        />
+      </div>
       <ViewInspection.Header
         templateName={inspectionData?.name}
         createdOn={inspectionData?.inspectionDate}
