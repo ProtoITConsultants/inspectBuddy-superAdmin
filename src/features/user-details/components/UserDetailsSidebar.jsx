3bab6c39cdf521a9c109542e0d095307fc273552
Babar78 <muhammadbabarashraf8@gmail.com>
2025-06-03 20:09:24 +0500

    feat: implement user details fetching and error handling in sidebar component

--- src/features/user-details/components/UserDetailsSidebar.jsx
+++ src/features/user-details/components/UserDetailsSidebar.jsx
@@ -1,21 +1,35 @@
-import { Link, useLocation, useParams } from "react-router";
+import { Link, useLocation, useNavigate, useParams } from "react-router";
 import { useUserDetailsStore } from "../../../store/userDetailsStore";
-import { useEffect, useMemo, useState } from "react";
+import React, { useCallback, useEffect, useMemo } from "react";
 import { USER_DETAILS_SIDEBAR } from "../../../constants/sidebarItems";
 import { Tooltip } from "@mantine/core";
+import { userDetailsAPIs } from "../api";
+import { useQuery } from "@tanstack/react-query";
+import { toast } from "sonner";
+import LoadingBackdrop from "./../../../components/ui/LoadingBackdrop";
 
 const UserDetailsSidebar = () => {
   // Hooks
   const location = useLocation();
-
-  // Local States
-  const [userRole, setUserRole] = useState("");
+  const navigate = useNavigate();
 
   // Global States
   const activeTab = useUserDetailsStore((state) => state.activeTab);
   const setActiveTab = useUserDetailsStore((state) => state.setActiveTab);
 
   const { userId } = useParams();
+
+  // Query To Fetch User Details
+  const fetchUserDetails = useCallback(
+    () => userDetailsAPIs.fetchUserProfileDetails({ userId }),
+    [userId]
+  );
+
+  const { data, error, isError, isPending } = useQuery({
+    queryKey: ["userProfileDetails", userId],
+    queryFn: fetchUserDetails,
+    enabled: !!userId, // Ensures query only runs when userId exists
+  });
 
   // Define a mapping of paths to active tab names
   const tabMapping = useMemo(
@@ -39,56 +53,58 @@ const UserDetailsSidebar = () => {
     }
   }, [location.pathname, activeTab, setActiveTab, tabMapping]);
 
-  const role = localStorage.getItem("userRole");
-  useEffect(() => {
-    setUserRole(role);
-
-    return () => {
-      setUserRole("");
-    };
-  }, [role]);
+  // Error Toast
+  if (isError) {
+    navigate(-1);
+    return toast.error("Error!", {
+      description: error.message || `Couldn't fetch User Details.`,
+      duration: 3000,
+    });
+  }
 
   return (
-    <div className="border-[1.5px] border-[#CCE2FF] rounded-[8px] bg-white px-[26.5px] py-[16px] h-fit w-[278px] fixed top-[196px] min-[992px]:flex flex-col gap-[8px] hidden">
-      {USER_DETAILS_SIDEBAR.map((page) => {
-        const isSubUsersDisabled =
-          page.pageTitle === "Sub Users" && userRole !== "TOPTIER";
-
-        return (
-          <Link
-            key={page.pageTitle}
-            to={
-              isSubUsersDisabled
-                ? "#"
-                : page.pageTitle === "User Details"
-                ? `/user-details/${userId}`
-                : `/user-details/${userId}/${page.pagePath}`
-            }
-            className={`w-full rounded-[4px] h-[40px] text-[16px] flex items-center !px-[16px] ${
-              activeTab === page.pageTitle
-                ? "bg-[rgba(135,184,254,0.12)] text-primary hover:text-darkPrimary hover:bg-[rgba(135,184,254,0.30)]"
-                : isSubUsersDisabled
-                ? "text-gray-400 cursor-not-allowed"
-                : "text-[#5A5A5A] hover:bg-gray-100"
-            }`}
-            onClick={() => {
-              if (!isSubUsersDisabled && activeTab !== page.pageTitle) {
-                setActiveTab(page.pageTitle);
+    <React.Fragment>
+      {isPending && <LoadingBackdrop />}
+      <div className="border-[1.5px] border-[#CCE2FF] rounded-[8px] bg-white px-[26.5px] py-[16px] h-fit w-[278px] fixed top-[196px] min-[992px]:flex flex-col gap-[8px] hidden">
+        {USER_DETAILS_SIDEBAR.map((page) => {
+          const isSubUsersDisabled =
+            page.pageTitle === "Sub Users" && data?.role !== "TOPTIER";
+          return (
+            <Link
+              key={page.pageTitle}
+              to={
+                isSubUsersDisabled
+                  ? "#"
+                  : page.pageTitle === "User Details"
+                  ? `/user-details/${userId}`
+                  : `/user-details/${userId}/${page.pagePath}`
               }
-            }}
-            aria-disabled={isSubUsersDisabled}
-          >
-            {isSubUsersDisabled ? (
-              <Tooltip label="User is not a 'TOPTIER' user.">
-                <p>{page.pageTitle}</p>
-              </Tooltip>
-            ) : (
-              page.pageTitle
-            )}
-          </Link>
-        );
-      })}
-    </div>
+              className={`w-full rounded-[4px] h-[40px] text-[16px] flex items-center !px-[16px] ${
+                activeTab === page.pageTitle
+                  ? "bg-[rgba(135,184,254,0.12)] text-primary hover:text-darkPrimary hover:bg-[rgba(135,184,254,0.30)]"
+                  : isSubUsersDisabled
+                  ? "text-gray-400 cursor-not-allowed"
+                  : "text-[#5A5A5A] hover:bg-gray-100"
+              }`}
+              onClick={() => {
+                if (!isSubUsersDisabled && activeTab !== page.pageTitle) {
+                  setActiveTab(page.pageTitle);
+                }
+              }}
+              aria-disabled={isSubUsersDisabled}
+            >
+              {isSubUsersDisabled ? (
+                <Tooltip label="User is not a 'TOPTIER' user.">
+                  <p>{page.pageTitle}</p>
+                </Tooltip>
+              ) : (
+                page.pageTitle
+              )}
+            </Link>
+          );
+        })}
+      </div>
+    </React.Fragment>
   );
 };
 
