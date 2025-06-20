5d768ecd4deec6f80b63b4f43e649d44d3bc2ef9
Babar78 <muhammadbabarashraf8@gmail.com>
2025-06-21 02:16:15 +0500

    feat: add LOGOUT_AUTH_URL to AUTH_ENDPOINTS and update logout service to use it

--- src/constants/api/auth.js
+++ src/constants/api/auth.js
@@ -1,10 +1,12 @@
 // API Endpoints
 const LOGIN_AUTH_URL = "/login";
 const CHECK_AUTH_URL = "/checkAdminAuthStatus";
+const LOGOUT_AUTH_URL = "/logout";
 
 const AUTH_ENDPOINTS = {
   LOGIN_AUTH_URL,
   CHECK_AUTH_URL,
+  LOGOUT_AUTH_URL,
 };
 
 export default AUTH_ENDPOINTS;
